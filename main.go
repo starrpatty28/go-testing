f4d1113f4b86184653dbc5122e12ef5281dc57de
starrpatty28 <starrpatty28@gmail.com>
2019-12-30 10:44:22 -0800

    Quick test on main.go file

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("Golang Testing")
+	result := Calculate(2)
+	fmt.Println(result)
+}
